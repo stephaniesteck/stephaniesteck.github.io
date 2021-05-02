89fc290e9ab013a0d9d42b10c9ccf0f26fbd207d
stephaniesteck <stephsteck@byui.edu>
2021-05-01 23:49:18 -0600

    Update

---
+++ design-principles.js
@@ -0,0 +1,14 @@
+let date = new Date();
+
+function writedate() {
+    // MM/DD/YYYY HH:MM:SS
+    let hour = date.getHours();
+    var hourText = hour;
+    var timeOfDay = 'AM';
+    if (hour > 12) {
+        hourText = hour - 12;
+        timeOfDay = 'PM';
+    }
+    document.getElementById("copyright").innerHTML = date.getFullYear();
+    document.getElementById("lastUpdate").innerHTML = document.lastModified;
+}
