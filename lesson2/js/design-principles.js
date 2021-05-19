ecfcf7a9e5f170d30b1fc314be2d8b0c27aaea63
stephaniesteck <stephsteck@byui.edu>
2021-05-18 23:55:36 -0600

    update

--- lesson2/js/design-principles.js
+++ lesson2/js/design-principles.js
@@ -1,16 +1,3 @@
-let date = new Date();
-
-function writedate() {
-    // MM/DD/YYYY HH:MM:SS
-    let hour = date.getHours();
-    var hourText = hour;
-    var timeOfDay = 'AM';
-    if (hour > 12) {
-        hourText = hour - 12;
-        timeOfDay = 'PM';
-    }
-    
+var strong = document.lastModified;
+document.querySelector('#lastmod').textContent = document.lastModified;
 document.querySelector('#copyright-year').innerHTML = new Date().getFullYear();
-    document.getElementById("lastUpdate").innerHTML = document.lastModified;
-}
-document.querySelector('#lastmod').textContent = document.lastModified
