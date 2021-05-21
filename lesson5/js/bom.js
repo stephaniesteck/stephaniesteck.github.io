dc86040dd4480b1ee8d4fbba1dae9cbd12a110bd
stephaniesteck <stephsteck@byui.edu>
2021-05-20 22:42:07 -0600

    started lesson 5

---
+++ lesson5/js/bom.js
@@ -0,0 +1,24 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const list = document.querySelector('ul')
+
+button.addEventListener('click', ()=>{
+    //store input value
+    let value = input.value;
+    //shows total number
+    letliCount =  document.getElementsByTagName('li').length+1;
+    //verify a value was added
+    if (!value.trim()){
+        console.log('nothing entered try again');
+    } else if (liCount > 25){
+        alert('you have reached the limit of 25 chapters. Please remove one before adding another chapter');
+    } else {
+
+    }
+});
+
+let li = documentcreateElement(' ');
+
+deleteButton.addEventListener('click', function(){
+    //code
+});
