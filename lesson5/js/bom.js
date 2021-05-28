4d4ae3008dac2922d961a3ecdc17568ea0b3b631
stephaniesteck <stephsteck@byui.edu>
2021-05-27 23:22:39 -0600

    update

--- lesson5/js/bom.js
+++ lesson5/js/bom.js
@@ -1,15 +1,20 @@
-function addChapter(){
-    let chapterNum = document.querySelector('#favchap').value;
-    
-    if (!chapterNum.trim()){
-        alert('nothing entered try again');
-    } else if (parseInt(chapterNum) > 25){
-        alert('you have reached the limit of 25 chapters. Please remove one before adding another chapter');
-    } else {
-        const liNew =document.createElement('li');
-        const btnNew =document.createElement('button');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const list = document.querySelector('ul');
 
-        
+button.addEventListener('click', function() {
+    const liNew = document.createElement('li');
+    const listText = document.createElement('span');
+    const btnNew = document.createElement('button');
+
+    liNew.appendChild(liNew);
+    listText.textContent = liNew;
+    liNew.appendChild(liNew);
+    btnNew.textContent = 'X';
+    list.appendChild(liNew);
+
+    btnNew.onclick =function(e) {
+        list.removeChild(liNew);
     }
-    
-}
+    input.focus();
+});
