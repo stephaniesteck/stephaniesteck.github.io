90f9d496b4dc5b6b358f5bb1c3f22dc5b3a84ca1
Lewis Lockhart <lewis00001@gmail.com>
2021-06-02 08:29:12 -0600

    made some minor adjustments

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,10 +1,10 @@
-const images =document.querySelectorAll("img[data-src");
+const images = document.querySelectorAll("img[data-src]");
 
 
 const imgOptions = {
-    threshold: 0 ,
-    rootMargin: 0px 0px 30px 0px;
-};
+    threshold: 0,
+    rootMargin: "0px 0px 30px 0px"
+}
 
 function preloadImage(img) {
     const src = img.getAttribute("data-src");
