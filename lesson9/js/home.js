e0249c715c685b182aa09a3560b0fa67d9dabbcf
stephaniesteck <stephsteck@byui.edu>
2021-06-24 09:39:53 -0600

    update

--- lesson9/js/home.js
+++ lesson9/js/home.js
@@ -49,7 +49,7 @@ fetch(requestURL)
             details.appendChild(yearFounded);
             details.appendChild(currentPopulation);
             details.appendChild(averageRainfall);
-            document.querySelector('section.home-towns').appendChild(card);
+            document.querySelector('section.home-towns').appendChild(home-towns);
         }
     }
 });
