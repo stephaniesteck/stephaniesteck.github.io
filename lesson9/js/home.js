4474156801c10ab0c53007f6147c7b6121437978
stephaniesteck <stephsteck@byui.edu>
2021-07-02 00:08:28 -0600

    update

--- lesson9/js/home.js
+++ lesson9/js/home.js
@@ -21,35 +21,96 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    
+    console.table(jsonObject);
+
+    let soda = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let motto = document.createElement('p');
+    let yearFounded = document.createElement('p');
+    let currentPopulation = document.createElement('p');
+    let averageRainfall = document.createElement('p');
+    soda.classList.add('soda-title');
+    h2.classList.add('soda-h2');
+    motto.classList.add('soda-motto');
+    yearFounded.classList.add('soda-year');
+    currentPopulation.classList.add('soda-pop');
+    averageRainfall.classList.add('soda-rain');
+
     const towns = jsonObject['towns'];
-    
-    for (let i = 0; i < towns.length; i++ ) {
-        if (towns[i].name == "Preston" || towns[i].name == "Fish Haven" || towns[i].name == "Soda Springs") {
-            let card = document.createElement('section');
-            let name = document.createElement('h2');
-            let motto = document.createElement('h4');
-            let image = document.createElement('img');
-            let yearFounded = document.createElement('p');
-            let currentPopulation = document.createElement('p');
-            let averageRainfall = document.createElement('p');
-            let details = document.createElement('section');
-            name.textContent = towns[i].name;
-            motto.textContent = towns[i].motto;
-            currentPopulation.textContent = 'Population: ' + towns[i].currentPopulation;
-            averageRainfall .textContent = 'Annual Rainfall: ' + towns[i].averageRainfall + ' ' + 'in.';
-            yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
-            image.setAttribute('src', '/lesson9/images/' + towns[i].photo);
-            image.setAttribute('alt', towns[i].name);
-            details.setAttribute('id', 'details');
-            card.appendChild(details);
-            card.appendChild(image);
-            details.appendChild(name);
-            details.appendChild(motto);
-            details.appendChild(yearFounded);
-            details.appendChild(currentPopulation);
-            details.appendChild(averageRainfall);
-            document.querySelector('section.home-towns').appendChild(cards);
-        }
-    }
+
+    h2.textContent = towns[2].name;
+    motto.textContent = towns[2].motto;
+    yearFounded.textContent = 'Year Founded' + towns[2].yearFounded;
+    currentPopulation.textContent = 'Current Population: ' + towns[2].currentPopulation;
+    averageRainfall.textContent = "Average Rainfall: " + towns[2].averageRainfall;
+
+    soda.appendChild(h2).appendChild(motto).appendChild(yearFounded).appendChild(currentPopulation).appendChild(averageRainfall);
+
+    document.querySelector('article.soda').appendChild(soda);
+});
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);
+
+    let fish = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let motto = document.createElement('p');
+    let yearFounded = document.createElement('p');
+    let currentPopulation = document.createElement('p');
+    let averageRainfall = document.createElement('p');
+    fish.classList.add('fish-title');
+    h2.classList.add('fish-h2');
+    motto.classList.add('fish-motto');
+    yearFounded.classList.add('fish-year');
+    currentPopulation.classList.add('fish-pop');
+    averageRainfall.classList.add('fish-rain');
+
+    const towns = jsonObject['towns'];
+
+    h2.textContent = towns[2].name;
+    motto.textContent = towns[2].motto;
+    yearFounded.textContent = 'Year Founded' + towns[2].yearFounded;
+    currentPopulation.textContent = 'Current Population: ' + towns[2].currentPopulation;
+    averageRainfall.textContent = "Average Rainfall: " + towns[2].averageRainfall;
+
+    fish.appendChild(h2).appendChild(motto).appendChild(yearFounded).appendChild(currentPopulation).appendChild(averageRainfall);
+
+    document.querySelector('article.fish').appendChild(fish);
+});
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);
+
+    let preston = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let motto = document.createElement('p');
+    let yearFounded = document.createElement('p');
+    let currentPopulation = document.createElement('p');
+    let averageRainfall = document.createElement('p');
+    preston.classList.add('preston-title');
+    h2.classList.add('preston-h2');
+    motto.classList.add('preston-motto');
+    yearFounded.classList.add('preston-year');
+    currentPopulation.classList.add('preston-pop');
+    averageRainfall.classList.add('preston-rain');
+
+    const towns = jsonObject['towns'];
+
+    h2.textContent = towns[2].name;
+    motto.textContent = towns[2].motto;
+    yearFounded.textContent = 'Year Founded' + towns[2].yearFounded;
+    currentPopulation.textContent = 'Current Population: ' + towns[2].currentPopulation;
+    averageRainfall.textContent = "Average Rainfall: " + towns[2].averageRainfall;
+
+    preston.appendChild(h2).appendChild(motto).appendChild(yearFounded).appendChild(currentPopulation).appendChild(averageRainfall);
+
+    document.querySelector('article.preston').appendChild(preston);
 });
