81a836d510ba4669b82c2f9c86eb4ee064a15854
stephaniesteck <stephsteck@byui.edu>
2021-07-06 00:46:33 -0600

    update

--- week11/js/events.js
+++ week11/js/events.js
@@ -5,18 +5,18 @@ fetch(townsRequestURL)
     return response.json();
   })
   .then(function (jsonObject) {
+     
     // holds json object data
     const towns = jsonObject['towns'];
 
     // get current page title 
-    const currentPage = document.querySelector('.page-title').textContent;
-    // trims the ' Idaho' from the title
-    const trimmedPageName = currentPage.slice(0, currentPage.length - 6);
+    const currentPage = document.querySelector('.preston').textContent;
 
     // loop though town list from the json data
+    console.log(currentPage)
     for (let i = 0; i < towns.length; i++ ) {
         // check to see if current town is in townList and matches current page create UI elements
-        if (towns[i].name == trimmedPageName) {
+        if (towns[i].name == currentPage) {
             outputEvents(towns[i].events);
         }
     }
@@ -28,7 +28,7 @@ function outputEvents(events) {
     // loop though the events and add them to the page
     for (let i = 0; i < events.length; i++) {
         // create element, class, content
-        let ev = `<p class="event-item"><i class="far fa-calendar-alt"></i>${events[i]}</p>`
+        let ev = `<p class="event-item"><i class="hidden"></i>${events[i]}</p>`
         // output to page
         eventList.insertAdjacentHTML('beforeend', ev);
     }
