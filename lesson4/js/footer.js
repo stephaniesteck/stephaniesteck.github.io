623cd1adf48db61594df325960a54c07133798ac
stephaniesteck <82967120+stephaniesteck@users.noreply.github.com>
2021-10-16 15:02:23 -0600

    update

--- lesson4/js/footer.js
+++ lesson4/js/footer.js
@@ -1,36 +1,17 @@
 const year =document.querySelector("#copyright")
 	year.innerHTML = new Date().getFullYear();
+	
+document.addEventListener('DOMContentLoaded', function() {        
+    // get and format current date 
+    let getDate = new Date();
+    let dayOptions = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric' 
+    };
+    let todaysDate = getDate.toLocaleDateString(undefined, dayOptions);
 
-const lastmod =document.querySelector("#lastmod");
-
-const days = [
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday"
-];
-const months = [
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December"
-];
-const d = new Date(document.lastModified);
-//const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
-const date = `${days[d.getDay()]}, ${months[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
-const last = document.querySelector("#lastmod");
-const dayNames = days[d.getDay()];
-const monthNames = months[d.getMonth()];
-const y = d.getFullYear();
-lastmod.innerHTML =date;
+    // update the last modified date and time
+    document.querySelector('#lastmod').textContent = todaysDate;   
+ }, false); 
