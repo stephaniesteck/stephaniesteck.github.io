3b34fd7361c5ea1791b8ef13cdf698077f111615
stephaniesteck <82967120+stephaniesteck@users.noreply.github.com>
2021-10-19 16:44:05 -0600

    update

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,3 +1,6 @@
-function toggleMenu() {
-    document.getElementsByClassName('.navigation')[0].classList.toggle('responsive');
-}
+const hambutton = document.querySelector('.ham-menu');
+const nav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {nav.classList.toggle('responsive')}, false);
+
+window.onresize =() => {if (window.innerWidth > 520) nav.classList.remove('responsive')};
