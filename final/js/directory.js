0ab75e33004660e21e85f81fbb68812dae461c40
stephaniesteck <82967120+stephaniesteck@users.noreply.github.com>
2021-12-14 15:35:46 -0700

    jsonupdate

--- final/js/directory.js
+++ final/js/directory.js
@@ -1,59 +1,78 @@
-const directory = "https://stephaniesteck.github.io/final/directory.json";
+const directory = "https://stephaniesteck.github.io/final/json/directory.json";
 
 fetch(directory)
     .then(function (response) {
         return response.json();
     })
-    .then(function (jsonObject) {
+    .then(function (jsonObject){
+        console.table(jsonObject)
 
-        const directory = jsonObject['businesses'];
+        const business = jsonObject["businesses"];
 
-        for (let i = 0; i < directory.length; i++) {
-            if(directory.includes(directory[i].name)) {
-            let busImage;
+        const businessList = ["The BLGD 178", "Casino Star Theatre", "Gunnison Market", "Donaldson Insurance Agency Copy Station", "Donaldson Insurance Agency", "Satterwhite Log Homes", "Lyle Young Welding"];
 
-            switch (directory[i].name) {
+       // businessList.sort(fuction(a,b){ return b.value - a.value});
+        console.log(businessList);
+
+        for (let i = 0; i <business.length; i++) {
+            if(businessList.includes(business[i].name)) {
+                let bizImage;
+
+            switch (business[i].name) {
                 case 'The BLDG 178':
-                    busImage ='../images/bldg1.jpg';
+                    bizImage ='../images/bldg1.jpg';
                 break;
-                case 'Casino Star Threatre':
-                    busImage = '../images/casino.jpg';
+                case 'Casino Star Theatre': 
+                    bizImage = '../images/casino.jpg';
                 break;
                 case 'Gunnison Market':
-                    busImage = '../images/gunnisonm.jpg';
+                    bizImage ='../images/gunnisonm.jpg';
                 break;
                 case 'Donaldson Insurance Agency Copy Station':
-                    busImage = '../images/copys.jpg';
+                    bizImage ='../images/copys.jpg';
                 break;
-                case 'Donaldson Insurance Agency':
-                    busImage = '../images/don.jpg';
+                case 'Donaldson Insurance Agency': 
+                    bizImage = '../images/don.jpg';
                 break;
                 case 'Satterwhite Log Homes':
-                    busImage = '../images/satter.jpg';
+                    bizImage ='../images/satter.jpg';
                 break;
                 case 'Lyle Young Welding':
-                    busImage = '../images/young.jpg';
+                    bizImage ='../images/young.jpg';
                 break;
-            }}
-            
-            let cardsb = document.createElement('section');
-            let name = document.createElement('h3');
-            let busImg = document.createElement('img');
+                
+            }
+            let bizDiv = document.createElement('div');
+            let textDiv = document.createElement('div');
+            let bizH3 = document.createElement('h3');
             let address = document.createElement('p');
             let phone = document.createElement('p');
-            let link = document.createElement('button');
+            let bizUrl = document.createElement('a');
+            let bImg = document.createElement('img');
 
-            name.textContent = `${directory[i].name}`;
-            address.textContent = `${directory[i].address}`;
-            phone.textContent = `${directory[i].phone}`;
-            link.src = directory[i].pageurl;
-            //busImg.src = busImage;
-            //busImg.alt = ` image of ${directory[i].name}`;
+            bizDiv.classList.add('biz-div');
+            textDiv.classList.add('text-div');
+            bizH3.classList.add('biz-h3');
+            address.classList.add('address');
+            phone.classList.add('phone');
+            bizUrl.classList.add('biz-btn');
+            bImg.classList.add('biz-img');
+
+            bizH3.textContent = business[i].name;
+            address.textContent = business[i].address;
+            phone.textContent = business[i].phone;
+            bizUrl.textContent = business[i].pageurl;
+            bImg.src =bizImage;
+            bImg.alt = `image of business ${business[i].name}`;
             
+            textDiv.appendChild(bizH3);
+            textDiv.appendChild(address);
+            textDiv.appendChild(phone);
+            textDiv.appendChild(bizUrl);
+            bizDiv.appendChild(textDiv);
+            bizDiv.appendChild(bImg);
 
-            cardsb.appendChild(name).appendChild(busImg).appendChild(address).appendChild(phone).appendChild(link);
-
-            document.querySelector('.cards').appendChild(cardsb);
+            document.querySelector('.cards').appendChild(bizDiv);
+            }
         }
-
-        })
+    });
